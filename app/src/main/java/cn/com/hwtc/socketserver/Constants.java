eb82bfec5c048a793407b04cb0dfd049c9b67b60
duji <jid@hwtc.com.cn>
2018-12-14 17:14:33 +0800

    将interface改为handler刷新UI

---
+++ app/src/main/java/cn/com/hwtc/socketserver/Constants.java
@@ -0,0 +1,11 @@
+package cn.com.hwtc.socketserver;
+
+/**
+ * user:Created by jid on 2018/12/14 16:59:01.
+ * email:18571762595@163.com.
+ */
+public class Constants {
+    public static final String TAG_BASE = "SocketServer ";
+    public static final int MSG_UPDATE_RECEIVE_MESSAGE = 1;
+    public static final String RECEIVE_MSG = "receive_msg";
+}
